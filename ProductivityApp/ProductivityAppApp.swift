df8825dded555fc9cbcf13671bdba42f902f3d83
akaplin23 <akaplin23@gmail.com>
2026-01-21 20:02:07 -0800

    Init Xcode

---
+++ ProductivityApp/ProductivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityAppApp.swift
+//  ProductivityApp
+//
+//  Created by Ava Kaplin on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
