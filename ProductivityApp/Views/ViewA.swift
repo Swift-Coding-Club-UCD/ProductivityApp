da03295a0bfe3ce64b6fa5c1b5f8b389354a4796
akaplin23 <akaplin23@gmail.com>
2026-01-25 21:45:20 -0800

    creating a tabview

---
+++ ProductivityApp/Views/ViewA.swift
@@ -0,0 +1,18 @@
+//
+//  ViewA.swift
+//  ProductivityApp
+//
+//  Created by Ava Kaplin on 1/25/26.
+//
+
+import SwiftUI
+
+struct ViewA: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewA()
+}
